51109fd5697c4cd177171cab4d29e840300e4173
Angela Sun <angelasun1995@gmail.com>
2022-04-20 16:06:39 -0400

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   devServer: {
     static: './dist',
   },
